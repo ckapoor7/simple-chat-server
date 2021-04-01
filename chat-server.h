fcda38b491a8fa84ae666545006a08af6dfe6d65
ckapoor7 <chaitanya.7.kapoor@gmail.com>
2021-04-01 22:29:04 +0530

    added guards and defined useful constants

--- chat-server.h
+++ chat-server.h
@@ -1,3 +1,6 @@
+#ifndef CHAT_SERVER
+#define CHAT_SERVER
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -10,3 +13,7 @@
 #include <arpa/inet.h>
 
 #define UNAMEMAX 50 //max length of an username
+#define PORT "3490" //port which the user connects to
+#define MAXDATASIZE 1000000 //max number of bytes that we can get at once (1MB)
+
+#endif
